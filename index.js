1ecd9b8ec8c0c7469d3ab549f76f32fc75ee37f3
Victoria <vmcdaniel459@gmail.com>
2024-09-10 12:06:52 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(records) {
+    const recordsForBowl = records.find(record => record.result === 'W')
+    if (recordsForBowl === undefined) {
+        return undefined
+    
+    } else return recordsForBowl.year
+    
+}
